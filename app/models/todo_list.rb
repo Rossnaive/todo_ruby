be68ca82c16db299cbd9750683e5c3bbd255f284
Rossnaive <ross.lee8888@gmail.com>
2017-12-06 16:35:15 +0700

    fixed dependent destroy

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,3 @@
 class TodoList < ApplicationRecord
-  has_many:todo_items
+  has_many:todo_items, :dependent => :destroy # dependent destroy will take care of removing associated children rows while removing a parent
 end
